3858d1a9c003d00d841c1e182ae141f1209a8310
Selvin Norman <selvin715@gmail.com>
2018-08-14 11:49:35 -0700

    Create a notesSchema and a Note Model

---
+++ models/note.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+});
+
+// Add `createdAt` and `updatedAt` fields
+noteSchema.set('timestamps', true);
+module.exports = mongoose.model('Note', noteSchema);
