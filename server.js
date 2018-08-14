1b6aa0e0609668b4810b8d35ced22cee4dea7a8f
Selvin Norman <selvin715@gmail.com>
2018-08-14 14:21:57 -0700

    add mongoose.connect

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 
 const express = require('express');
 const morgan = require('morgan');
-
+const mongoose = require('mongoose');
 const { PORT } = require('./config');
+const { PORT, MONGODB_URI } = require('./config');
 
 const notesRouter = require('./routes/notes');
 
@@ -42,6 +43,23 @@ app.use((err, req, res, next) => {
   }
 });
 
+// Connect to DB and Listen for incoming connections
+mongoose.connect(MONGODB_URI)
+  .then(instance => {
+    const conn = instance.connections[0];
+    console.info(`Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`);
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error('\n === Did you remember to start `mongod`? === \n');
+    console.error(err);
+  });
+
+app.listen(PORT, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
 // Listen for incoming connections
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, function () {
