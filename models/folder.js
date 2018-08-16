0bacc01ccf583f55846bcc3e4409b28dd5357b52
Selvin Norman <selvin715@gmail.com>
2018-08-16 12:18:04 -0700

    Seeded DB

---
+++ models/folder.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const folderSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  content: String
+});
+
+// Add `createdAt` and `updatedAt` fields
+folderSchema.set('timestamps', true);
+folderSchema.set('toObject', {
+  virtuals: true,     // include built-in virtual `id`
+  versionKey: false,  // remove `__v` version key
+  transform: (doc, ret) => {
+    delete ret._id; // delete `_id`
+  }
+});
+module.exports = mongoose.model('Folder', folderSchema);
